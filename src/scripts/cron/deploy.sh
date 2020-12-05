dd2ff0bdd29fa7f4f7f298d8c47922d6331883d9
Anand Bonde <anand.bonde@gmail.com>
2020-12-05 00:52:13 -0800

    Changed credentials and deleted them from here

--- src/scripts/cron/deploy.sh
+++ src/scripts/cron/deploy.sh
@@ -4,7 +4,7 @@ cd ~/git/covid_19_dashboard/src/R/covid_19_status
 
 echo "[`date`] Starting shiny app deployment"
 
-R -e "rsconnect::setAccountInfo(name='curiotiveme', token='59D8786686492D3EB70C7C5B0ADB413A', secret='5grwmaqmkrjvCdvzzNkJtVuk2yf2NJBHfnnpuaxW')" > /dev/null 2>&1
+R -e "rsconnect::setAccountInfo(name='', token='', )" > /dev/null 2>&1
 
 R -e "rsconnect::deployApp()" > /dev/null 2>&1
 
